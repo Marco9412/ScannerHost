1e51f857847fd1e0d278bfb893d3f1b8de596412
Marco Panato <marco.panato94@gmail.com>
2019-03-02 00:21:36 +0100

    Added SCANNER_HOST_BASEPATH to store docs

--- utils.py
+++ utils.py
@@ -7,16 +7,22 @@ import subprocess
 # The name can be extracted by executing 'scanimage -L'
 SCANNER_DEVICE = None  #'epson2:libusb:001:003'
 
+# os.environ['SCANNER_HOST_PORT']
+
+BASEPATH = os.environ['SCANNER_HOST_BASEPATH'] if 'SCANNER_HOST_BASEPATH' in os.environ else '.'
+SESSIONS_FOLDER = BASEPATH + '/sessions'
+DOCUMENTS_FOLDER = BASEPATH + '/documents'
+
 
 def check_folder():
-    if not os.path.isdir('./sessions'):
-        os.mkdir('./sessions')
-    if not os.path.isdir('./documents'):
-        os.mkdir('./documents')
+    if not os.path.isdir(SESSIONS_FOLDER):
+        os.mkdir(SESSIONS_FOLDER)
+    if not os.path.isdir(DOCUMENTS_FOLDER):
+        os.mkdir(DOCUMENTS_FOLDER)
 
 
 def scan(session_id):
-    directory = './sessions/%s' % session_id
+    directory = '%s/%s' % (SESSIONS_FOLDER, session_id)
 
     if not os.path.isdir(directory):
         os.mkdir(directory)
@@ -24,7 +30,7 @@ def scan(session_id):
     num = len(os.listdir(directory))
     if SCANNER_DEVICE:
         proc = subprocess.run(
-            ['/bin/bash', '-c', 'scanimage --resolution 300 -d %s --format=png > %s/%d.png'
+            ['/bin/sh', '-c', 'scanimage --resolution 300 -d %s --format=png > %s/%d.png'
              % (SCANNER_DEVICE, directory, num)])
         return ('%s/%d.png' % (directory, num)), proc.returncode == 0
     else:
@@ -34,8 +40,8 @@ def scan(session_id):
 
 
 def pdf(session_id):
-    directory = './sessions/%s' % session_id
-    output = './documents/%s' % session_id
+    directory = '%s/%s' % (SESSIONS_FOLDER, session_id)
+    output = '%s/%s' % (DOCUMENTS_FOLDER, session_id)
 
     if not os.path.isdir(directory):
         return None, False
@@ -47,5 +53,6 @@ def pdf(session_id):
     for n in range(0, num):
         res = res + '%s/%d.png ' % (directory, n)
 
-    proc = subprocess.run(['/bin/bash', '-c', 'convert %s %s/document.pdf' % (res, output)])
+    proc = subprocess.run(['/bin/sh', '-c', 'convert %s %s/document.pdf' % (res, output)])
     return ('%s/document.pdf' % output), proc.returncode == 0
+
